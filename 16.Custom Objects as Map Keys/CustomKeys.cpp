ef73603e00dee0268a1f308a6f8ce5c24a29bd8a
srcarrel <s.r.carrell@gmail.com>
2018-02-28 13:57:11 -0500

    16.Custom Objects as Map Keys

---
+++ 16.Custom Objects as Map Keys/CustomKeys.cpp
@@ -0,0 +1,54 @@
+//Name: CustomKeys.cpp
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+class Person {
+    private:
+        string name;
+        int age;
+
+    public:
+
+        Person(): name(""), age(0) {}
+
+        Person(const Person &other) {
+            name = other.name;
+            age = other.age;
+        }
+
+        Person(string name, int age) : name(name), age(age) {
+
+        }
+
+        void print() const {
+            cout << name << ": " << age << flush;
+        }
+
+        bool operator<(const Person &other) const {
+            if ( name == other.name ) {
+                return age < other.age;
+            } else {
+                return name < other.name;
+            }
+        }
+};
+
+int main() {
+
+    map<Person, int> people;
+
+    people[Person("Mike", 40)] = 40;
+    people[Person("Mike", 777)] = 35;
+    people[Person("Sue", 30)] = 30;
+    people[Person("Raj", 20)] = 20;
+
+    for (map<Person, int>::iterator it = people.begin(); it != people.end(); it++) {
+        cout << it->second << ": " << flush;
+        it->first.print();
+        cout << endl;
+    }
+
+    return 0;
+}
