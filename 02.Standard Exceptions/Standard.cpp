02be314c3552cd481f2107bc2c936d8c4f347159
srcarrel <s.r.carrell@gmail.com>
2018-02-28 09:43:28 -0500

    02.Standard Exceptions

---
+++ 02.Standard Exceptions/Standard.cpp
@@ -0,0 +1,25 @@
+// Name: Standard.cpp
+
+#include <iostream>
+using namespace std;
+
+class CanGoWrong {
+    public:
+        CanGoWrong() {
+            char *pMemory = new char[999999999999999999];
+            delete [] pMemory;
+        }
+};
+
+int main() {
+    try {
+        CanGoWrong wrong;
+    }
+    catch(bad_alloc &e) {
+        cout << "Caught exception: " << e.what() << endl;
+    }
+
+    cout << "Still running." << endl;
+
+    return 0;
+}
