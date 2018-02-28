809b3f04f7bf3ec1af31b0c99d6d1f2dc6d59b98
srcarrel <s.r.carrell@gmail.com>
2018-02-28 14:42:56 -0500

    20.Sorting Vectors

---
+++ 20.Sorting Vectors/Sorting.cpp
@@ -0,0 +1,45 @@
+//Name: Sorting.cpp
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Test {
+    int id;
+    string name;
+
+    public:
+    Test(int id, string name): id(id), name(name) {}
+    void print() {
+        cout << id << ": " << name << endl;
+    }
+
+    //bool operator<(const Test& other) const {
+    //    return name < other.name;
+    //}
+    
+    friend bool comp(const Test &a, const Test &b);
+};
+
+bool comp(const Test &a, const Test &b) {
+    return a.name < b.name;
+}
+
+int main() {
+    vector<Test> tests;
+
+    tests.push_back(Test(5, "Mike"));
+    tests.push_back(Test(10, "Sue"));
+    tests.push_back(Test(7, "Raj"));
+    tests.push_back(Test(3, "Vicky"));
+
+    sort(tests.begin(), tests.end(), comp);
+
+    for(int i = 0; i < tests.size(); i++) {
+        tests[i].print();
+    }
+
+    return 0;
+}
